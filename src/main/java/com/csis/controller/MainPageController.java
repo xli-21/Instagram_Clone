9f1d37a21b750ebcd16832867c0dc5a6f673814c
xli-21 <liux44@student.douglascollege.ca>
2020-12-06 22:50:22 -0800

    update to springboot

--- src/main/java/com/csis/controller/MainPageController.java
+++ src/main/java/com/csis/controller/MainPageController.java
@@ -1,5 +1,9 @@
 package com.csis.controller;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.csis.dao.ImageDaoImpl;
+import com.csis.dao.PageContentDaoImpl;
 import com.csis.dao.UserAccountDaoImpl;
+import com.csis.model.Follow;
+import com.csis.model.Image;
 import com.csis.model.UserAccount;
 
 @Controller
@@ -16,11 +25,37 @@ public class MainPageController {
 	@Autowired
 	UserAccountDaoImpl userDaoImpl;
 	
+	@Autowired
+	ImageDaoImpl imageDaoImpl;
+	
+	@Autowired
+	PageContentDaoImpl pageContentDaoImpl;
+	
+
 	@RequestMapping(value = "mainPage", method = RequestMethod.GET)
 	 public String showMainPage(ModelMap model,HttpSession session) {
 		UserAccount user = (UserAccount) session.getAttribute("user");
+		
+		
 		model.addAttribute("name", user.getFirstname());
 		model.addAttribute("mainPage", user);
+		
+		
+		
+		List<Follow> followingList = pageContentDaoImpl.followingList(user.getUsername());	
+		if(followingList.size()>0) {
+			List<Image> showList = new ArrayList<Image>();		
+			for (Follow temp : followingList) {
+				List<Image> imageList = imageDaoImpl.loadImage(temp.getHostusername());
+				if(imageList.size()>0) {
+					Image tempImage = imageList.get(imageList.size()-1);
+					showList.add(tempImage);
+				}
+	        }
+			showList.sort(Comparator.comparing(Image::getUploadDate).reversed());
+			model.addAttribute("showList", showList);
+		}
+		
 		return "mainPage";
 	 }
 	
